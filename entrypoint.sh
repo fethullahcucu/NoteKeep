09b1456fba4e594a7cc9f12fdcc33b4d438aca62
fethullahcucu <121039992+fethullahcucu@users.noreply.github.com>
2026-01-13 18:26:28 +0000

    kubernetes update
    kubernetes update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,11 @@ apt-get update
 apt-get install -y --no-install-recommends netcat-openbsd
 rm -rf /var/lib/apt/lists/*
 
-while ! nc -z db 27017; do
+echo "Database Starting...."
+
+while ! nc -z service-metada-name-mongodb.mongodb.svc.cluster.local 27017; do
     sleep 1
 done
 echo "Database Ready...."
 exec java -jar /app/app.jar "$@"
+    
