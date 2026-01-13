061ce392ff839fdd805aa7dc39969332633e41e7
fethullahcucu <121039992+fethullahcucu@users.noreply.github.com>
2026-01-13 18:07:29 +0000

    Update database connection check in entrypoint.sh
    kubernetes changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y --no-install-recommends netcat-openbsd
 rm -rf /var/lib/apt/lists/*
 
-while ! nc -z db 27017; do
+while ! nc -z service-metada-name-mongodb.mongodb.svc.cluster.local 27017; do
     sleep 1
 done
 echo "Database Ready...."
